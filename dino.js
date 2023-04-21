66d7c766e79c8a9721ec4b8b49d7d3f707f3ac4f
Dino Sim <dino@nonegolab.com>
2023-04-21 16:14:37 +0900

    dino first commit

---
+++ dino.js
@@ -0,0 +1,25 @@
+const music = [
+    {
+        singer: '정준일',
+        title: '보고싶었어요',
+        genre: 'k-pop'
+    },
+    {
+        singer: '윤종신',
+        title: '나이',
+        genre: 'k-pop'
+    },
+
+    {
+        singer: 'Ahmad Jamal',
+        title: 'I Love music',
+        genre: 'jazz'
+    },
+
+    {
+        singer: 'Nas',
+        title: 'Half-time',
+        genre: 'hiphop'
+    },
+
+]
