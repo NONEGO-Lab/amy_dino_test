b155f45317044d2725edaf6ec20acdb8aeed34a2
NONEGO-Lab <dev@nonegolab.com>
2023-04-21 16:19:56 +0900

    package.json 세팅

--- amy.js
+++ amy.js
@@ -1,4 +1,21 @@
+import { music } from "./dino.js";
+
 
 export const amy_food_list = [
     'zzazangmeon', 'nogari', 'beers'
+];
+
+export const amy_favorite_musics = [
+    {
+        singer: '아이유',
+        title: '에잇',
+        genre: 'k-pop'
+    },
+];
+
+export const nonego_play_list = [
+    ...music,
+    amy_favorite_musics,
 ]
+
+console.log(nonego_play_list);
